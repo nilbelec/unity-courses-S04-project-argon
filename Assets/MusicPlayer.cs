4fce562c039785d430d48b4eb8671d037d975e19
Juan Alvarez Curcho <celeblin@gmail.com>
2018-06-17 21:41:18 +0200

    S4_L80: Added player ship and Splash screen

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("LoadFirstLevel", 10f);
+	}
+
+    void LoadFirstLevel(){
+        SceneManager.LoadScene(1);
+    }
+}
