dba8e1a4d5ac00bfd8ffaeaf2a2e28c0362c7b53
Juan Álvarez <jalvarezcurcho@gmail.com>
2018-09-11 21:06:22 +0200

    S4_L86: Added clamped movement to the ship

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,19 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour {
 
     [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+    [Tooltip("In m")] [SerializeField] float xRange = 6f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float newXPosition = transform.localPosition.x + xOffset;
+        newXPosition = Mathf.Clamp(newXPosition, -xRange, xRange);
+        transform.localPosition = new Vector3(newXPosition, transform.localPosition.y, transform.localPosition.z);
 	}
 }
