3e9a23850cc1caff39373ac6827566f6587e5337
Juan Álvarez <jalvarezcurcho@gmail.com>
2018-09-11 20:49:56 +0200

    S4_L85

---
+++ Assets/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
+	}
+}
