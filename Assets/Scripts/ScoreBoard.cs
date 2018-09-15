9709ba42556ac6e202bf007d2a1e279b5bb4de82
Juan Álvarez <jalvarezcurcho@gmail.com>
2018-09-15 20:32:03 +0200

    L4_L105: Started to implement the Score Board

---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour {
+
+    int score;
+    Text scoreText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        scoreText = GetComponent<Text>();
+        UpdateScoreText();
+    }
+
+    void UpdateScoreText()
+    {
+        scoreText.text = score.ToString().PadLeft(8, '0');
+    }
+
+    public void ScoreHit (int hitScore) {
+        if (hitScore < 0)
+            return;
+        score += hitScore;
+        UpdateScoreText();
+    }
+}
