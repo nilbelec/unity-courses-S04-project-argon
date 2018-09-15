59f5f96fd3ba326ae057a36249f0217b8018042e
Juan Álvarez <jalvarezcurcho@gmail.com>
2018-09-15 13:05:56 +0200

    S4_L101: Improved imported assets prefabs using

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,10 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
-    private void OnParticleCollision(GameObject other)
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    void AddNonTriggerBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
     {
         Destroy(gameObject);
     }
