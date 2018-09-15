9f2d67f0e3230c54a6a7316bec879ec03f0a032e
Juan Álvarez <jalvarezcurcho@gmail.com>
2018-09-15 21:35:41 +0200

    S4_L106: Added score points after destroying enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,17 @@ public class Enemy : MonoBehaviour {
 
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePoints = 10;
+
+    ScoreBoard scoreBoard;
 
     void Start()
+    {
+        AddNonTriggerBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddNonTriggerBoxCollider()
     {
         Collider boxCollider = gameObject.AddComponent<BoxCollider>();
         boxCollider.isTrigger = false;
@@ -13,6 +22,7 @@ public class Enemy : MonoBehaviour {
 
     void OnParticleCollision(GameObject other)
     {
+        scoreBoard.ScoreHit(scorePoints);
         Instantiate(deathFX, transform.position, Quaternion.identity, parent);
         Destroy(gameObject);
     }
